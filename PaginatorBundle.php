4e662d5ff0cf5a103a815f3cbd704b8b1039abf9
Robert Tytko <kontakt@roberttytko.pl>
2019-11-11 18:58:31 +0100

    create paginator bundle

---
+++ PaginatorBundle.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Rareza\PaginatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PaginatorBundle extends Bundle
+{ }
---
+++ Utils/Paginator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rareza\PaginatorBundle\Utils;
+
+class Paginator
+{
+    public function test()
+    {
+        return 'Paginator ready';
+    }
+}
